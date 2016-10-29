ca92afd8b62d22c0acd6eb92efef25323ecff0fd
Vallie Joseph <valliejoseph@gmail.com>
2016-10-29 11:00:52 -0400

    Added basic jquery scraper template

--- scraper.js
+++ scraper.js
@@ -1,1 +1,12 @@
 console.log(document.getElementbyClass("titleText"));
+$.ajax({
+     url: "https://news.google.com/?ar=1477752873",
+     dataType: 'text',
+     success: function(data) {
+          var elements = $("<div>").html(data)[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
+          for(var i = 0; i < elements.length; i++) {
+               var theText = elements[i].firstChild.nodeValue;
+              console.log(theText);
+          }
+     }
+});
